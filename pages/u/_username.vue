5e4fa22c4d7f5d2625b710a0604202c880d112aa
Karim Daghari <karimdxy@gmail.com>
2019-12-27 19:45:10 +0100

    feat(profile): add missing stuff

--- pages/u/_username.vue
+++ pages/u/_username.vue
@@ -2,26 +2,15 @@
   <v-card>
     <v-container>
       <v-row>
-        <v-col offset="3" cols="9" sm="2" offset-sm="0">
-          <v-avatar color="blue" size="128"></v-avatar>
-          <div class="mt-2 ml-5 ml-sm-2 ml-md-5">
-            <v-btn icon href="https://facebook.com" small>
-              <v-icon>mdi-facebook-box</v-icon>
-            </v-btn>
-            <v-btn icon href="https://twitter.com" small>
-              <v-icon>mdi-twitter-box</v-icon>
-            </v-btn>
-            <v-btn icon href="https://linkedin.com" small>
-              <v-icon>mdi-linkedin-box</v-icon>
-            </v-btn>
-          </div>
+        <v-col cols="3" sm="2">
+          <v-avatar color="blue" size="96"></v-avatar>
         </v-col>
-        <v-col cols="12" sm="10">
+        <v-col cols="9" sm="10" class="ml-md-n12 mt-n4">
           <v-card-text class="mt-n2">
             <v-row class="mb-n5" justify="start">
               <v-col cols="9">
                 <h2>John Doe</h2>
-                <p><v-icon small>mdi-school</v-icon> ESPRIT</p>
+                <span> <v-icon dense>mdi-school</v-icon> Collège LaSalle </span>
               </v-col>
               <v-col v-if="isOwner()" cols="3">
                 <div style="float: right">
@@ -29,12 +18,83 @@
                 </div>
               </v-col>
             </v-row>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam
-              ipsum veritatis cumque? Nobis, dolorem? Aut aperiam ipsum dolore
-              beatae laudantium sunt, quo ea veniam repudiandae deserunt
-              architecto, itaque fuga quis.
-            </p>
+            <v-row>
+              <v-col>
+                <p class="text-justify">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Totam ipsum veritatis cumque? Nobis, dolorem? Aut aperiam
+                  ipsum dolore beatae laudantium sunt, quo ea veniam repudiandae
+                  deserunt architecto, itaque fuga quis.
+                </p>
+              </v-col>
+            </v-row>
+            <v-row dense no-gutters class="ml-n1 mt-n1">
+              <v-col>
+                <v-btn
+                  icon
+                  href="https://facebook.com"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  small
+                >
+                  <v-icon>mdi-web</v-icon>
+                </v-btn>
+                <v-btn
+                  icon
+                  href="https://facebook.com"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  small
+                >
+                  <v-icon>mdi-facebook-box</v-icon>
+                </v-btn>
+                <v-btn
+                  icon
+                  href="https://twitter.com"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  small
+                >
+                  <v-icon>mdi-twitter-box</v-icon>
+                </v-btn>
+                <v-btn
+                  icon
+                  href="https://linkedin.com"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  small
+                >
+                  <v-icon>mdi-linkedin-box</v-icon>
+                </v-btn>
+                <v-btn
+                  icon
+                  href="https://medium.com"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  small
+                >
+                  <v-icon>mdi-medium</v-icon>
+                </v-btn>
+                <v-btn
+                  icon
+                  href="https://instagram.com"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  small
+                >
+                  <v-icon>mdi-instagram</v-icon>
+                </v-btn>
+                <v-btn
+                  icon
+                  href="https://youtube.com"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  small
+                >
+                  <v-icon>mdi-youtube</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
           </v-card-text>
         </v-col>
       </v-row>
@@ -53,13 +113,33 @@
 
           <v-tab-item value="products">
             <v-container>
-              <p>Products</p>
+              <template v-for="i in 3">
+                <product-card
+                  :key="i"
+                  :product-link="`test-${i}`"
+                  :product-name="`test ${i}`"
+                  :product-tagline="'tagline'"
+                  :product-votes-count="42"
+                  :product-comments-count="42"
+                  class="my-products"
+                ></product-card>
+              </template>
             </v-container>
           </v-tab-item>
 
           <v-tab-item value="upvotes">
             <v-container>
-              <p>Upvotes</p>
+              <template v-for="i in 3">
+                <product-card
+                  :key="i"
+                  :product-link="`test-${i}`"
+                  :product-name="`test ${i}`"
+                  :product-tagline="'tagline'"
+                  :product-votes-count="42"
+                  :product-comments-count="42"
+                  class="my-products"
+                ></product-card>
+              </template>
             </v-container>
           </v-tab-item>
 
@@ -75,13 +155,32 @@
 </template>
 
 <script>
+import ProductCard from '@/components/ProductItemFeed.vue'
+
 export default {
+  components: {
+    ProductCard
+  },
   methods: {
     isOwner() {
-      const currentUser = this.$auth.user.username
-      const currentUserProfile = this.$route.params.username
-      return currentUser === currentUserProfile
+      if (this.$auth.loggedIn) {
+        const currentUser = this.$auth.user.username
+        const currentUserProfile = this.$route.params.username
+        return currentUser === currentUserProfile
+      } else {
+        return false
+      }
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.my-products {
+  margin-bottom: 1em;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+</style>
