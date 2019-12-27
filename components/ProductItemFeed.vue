561f23442c456b165858796a11aa889b0b3f8249
Karim Daghari <karimdxy@gmail.com>
2019-12-27 19:44:56 +0100

    fix(product-feed-item): adjust overall look

--- components/ProductItemFeed.vue
+++ components/ProductItemFeed.vue
@@ -1,60 +1,41 @@
 <template>
   <v-card :to="`/p/${productLink}`" nuxt>
     <v-container>
-      <v-row dense align="baseline">
+      <v-row dense align="center">
         <v-col cols="3" sm="1">
-          <v-avatar color="primary" size="72" class="ml-sm-n1"></v-avatar>
-          <v-row class="pl-md-2 d-none d-sm-flex">
-            <div v-for="i in 4" :key="i">
-              <product-makers-avatars
-                :makerName="'maker name'"
-                :makerProfileLink="'maker link'"
-              ></product-makers-avatars>
-            </div>
-          </v-row>
+          <v-avatar color="primary" size="72"></v-avatar>
         </v-col>
-        <v-col>
+        <v-col justify-self="start">
           <v-card-title v-text="productName"></v-card-title>
           <v-card-subtitle v-text="productTagline"></v-card-subtitle>
-          <v-card-text>
-            <v-row class="d-sm-none">
-              <div v-for="i in 4" :key="i">
-                <product-makers-avatars
-                  :makerName="'maker name'"
-                  :makerProfileLink="'maker link'"
-                ></product-makers-avatars>
-              </div>
-            </v-row>
+          <v-card-text class="ml-n1">
+            <span v-for="i in 4" :key="i">
+              <product-makers-avatars
+                :makerName="'maker name'"
+                :makerProfileLink="`maker-link-${i}`"
+              ></product-makers-avatars>
+            </span>
           </v-card-text>
         </v-col>
-        <v-col
-          cols="6"
-          sm="2"
-          offset-sm="0"
-          offset="6"
-          class="d-none d-sm-flex"
-        >
+
+        <v-col cols="3" sm="2">
           <v-card-actions>
-            <v-btn @click.prevent icon tile outlined height="50" width="50">
-              <div class="flex-md-column">
-                <v-icon>mdi-arrow-up-bold</v-icon>
-                <div v-text="productVotesCount"></div>
-              </div>
-            </v-btn>
-            <v-btn
-              :to="`/p/${productLink}#comments`"
-              nuxt
-              icon
-              tile
-              outlined
-              height="50"
-              width="50"
-            >
-              <div class="flex-md-column">
-                <v-icon>mdi-comment-text</v-icon>
-                <div v-text="productCommentsCount"></div>
-              </div>
-            </v-btn>
+            <v-row dense>
+              <v-col cols="12">
+                <v-btn @click.prevent color="orange" outlined block height="50">
+                  <div class="flex-md-column">
+                    <v-icon>mdi-arrow-up-thick</v-icon>
+                    <div v-text="productVotesCount"></div>
+                  </div>
+                </v-btn>
+              </v-col>
+              <v-col cols="12">
+                <v-btn :to="`/p/${productLink}#comments`" nuxt outlined block>
+                  <v-icon left>mdi-comment</v-icon>
+                  <span v-text="productCommentsCount"></span>
+                </v-btn>
+              </v-col>
+            </v-row>
           </v-card-actions>
         </v-col>
       </v-row>
