01852fc644d2f8d51768b820a225ef46e8768f19
Karim <karim_daghari@outlook.com>
2019-12-30 22:55:48 +0100

    feat(comment): add message for non-users

--- components/ProductPageCommentAdd.vue
+++ components/ProductPageCommentAdd.vue
@@ -1,7 +1,13 @@
 <template>
   <v-row dense>
     <v-col cols="12" md="10">
-      <v-textarea dense outlined auto-grow rows="1"></v-textarea>
+      <v-textarea
+        :placeholder="placeholder"
+        dense
+        outlined
+        auto-grow
+        rows="1"
+      ></v-textarea>
     </v-col>
     <v-col>
       <v-btn block color="orange" large class="mt-n5 mt-md-0">
@@ -13,6 +19,13 @@
 
 <script>
 export default {
-  name: 'ProductPageCommentAdd'
+  name: 'ProductPageCommentAdd',
+  computed: {
+    placeholder() {
+      const isLoggedIn = this.$auth.loggedIn
+      if (isLoggedIn) return ''
+      else return 'Please login or signup to add a comment'
+    }
+  }
 }
 </script>
