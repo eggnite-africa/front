10879d5a9c6063c991d465c7ba4781afd9cae291
Karim <karim_daghari@outlook.com>
2020-01-31 18:16:39 +0100

    style: adjust text

--- pages/guidelines.vue
+++ pages/guidelines.vue
@@ -5,7 +5,7 @@
         <v-img src="/checklist.svg" max-width="500" class="mx-auto"></v-img>
       </v-col>
       <v-col cols="12">
-        <header class="headline">Posting guidelines *</header>
+        <header class="headline">Posting guidelines*</header>
         <p class="subtitle-1 grey--text">
           Eggnite is for new or unknown products
         </p>
@@ -20,10 +20,9 @@
           </li>
           <li>Product has to be either in production or in a 'ready' state</li>
         </ul>
-        <v-subheader class="overline"
-          >*Disrespect of the guidelines may result in removal of the
-          product</v-subheader
-        >
+        <v-subheader class="overline font-weight-light text-lowercase">
+          *Disrespect of the guidelines may result in removal of the product
+        </v-subheader>
       </v-col>
       <v-col cols="12"> </v-col>
     </v-row>
