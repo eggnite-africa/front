1df0295f148ad8404ef067c6d9bbdffff9542111
Karim <karim_daghari@outlook.com>
2020-02-01 15:40:46 +0100

    feat(wip): add welcome banner

---
+++ components/WelcomeBanner.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row class="primary">
+    <v-col>
+      <header class="title">Welcome to Eggnite,</header>
+      <p class="subtitle-1">
+        Discover new products made by Tunisian student developers &
+        entrepreneurs
+      </p>
+      <v-btn color="green" depressed nuxt to="/join">Join us</v-btn> and become
+      part of an awesome community.
+      <nuxt-link to="/about" class="white--text">Learn more</nuxt-link>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'WelcomeBanner'
+}
+</script>
+
+<style lang="scss" scoped>
+.homepage-link {
+  text-decoration: none;
+}
+</style>
