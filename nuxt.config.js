eaf08d97ed0b061fbac271d4c30387de2f56d03f
Karim <karim_daghari@outlook.com>
2020-02-16 20:38:49 +0100

    chore: don't need to load before mounting

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['@/plugins/vuelidate', '@/plugins/vue-mugen-scroll'],
+  plugins: ['@/plugins/vuelidate'],
   /*
    ** Nuxt.js dev-modules
    */
--- plugins/vue-mugen-scroll.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import MugenScroll from 'vue-mugen-scroll'
-
-Vue.use(MugenScroll)
