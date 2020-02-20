46e38bd12755c236f5d4bb8b4254fcc56c9b8799
Karim <karim_daghari@outlook.com>
2020-02-20 16:07:00 +0100

    update copy

--- pages/guidelines.vue
+++ pages/guidelines.vue
@@ -5,7 +5,7 @@
         <v-img src="/checklist.svg" max-width="500" class="mx-auto"></v-img>
       </v-col>
       <v-col cols="12">
-        <header class="headline">Posting guidelines*</header>
+        <header class="headline">Posting guidelines<sup>*</sup></header>
         <p class="subtitle-1 grey--text">
           Eggnite is for new or unknown products
         </p>
@@ -14,14 +14,11 @@
             Product can be an app, a website, an open-source project, or a
             hardware-based one
           </li>
-          <li>
-            If product is hardware-based, a website has to be provided and at
-            least one picture should be included
-          </li>
           <li>Product has to be in a <strong>useable</strong> state</li>
         </ul>
         <v-subheader class="overline font-weight-light text-lowercase">
-          *Disrespect of the guidelines may result in removal of the product
+          <sup>*</sup>Disrespect of the guidelines may result in removal of the
+          product
         </v-subheader>
       </v-col>
       <v-col cols="12"> </v-col>
