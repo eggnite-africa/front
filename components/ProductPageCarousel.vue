fa31721ae2b8e6d2e28c5b9b21f9f239063724d7
Karim <karim_daghari@outlook.com>
2020-02-20 18:31:57 +0100

    style: image size should adapt to device frame size

--- components/ProductPageCarousel.vue
+++ components/ProductPageCarousel.vue
@@ -6,7 +6,7 @@
     height="100%"
   >
     <v-carousel-item v-for="(picture, i) in pictures" :key="i" eager>
-      <v-img :src="picture" eager contain></v-img>
+      <v-img :src="picture" :aspect-ratio="imageSize" eager contain></v-img>
     </v-carousel-item>
   </v-carousel>
 </template>
@@ -18,6 +18,12 @@ export default {
     pictures: {
       type: Array,
       required: true
+    }
+  },
+  computed: {
+    imageSize() {
+      if (this.$device.isDesktop) return '1.95'
+      return ''
     }
   }
 }
