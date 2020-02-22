0a7419f5b9ba50d8e3d56f62886f3b0e7c0d2492
Karim <karim_daghari@outlook.com>
2020-02-22 22:46:47 +0100

    fix: remove welcome banner
    BREAKING CHANGE: use welcome page instead of banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,6 @@
 <template>
   <div>
     <v-container>
-      <v-dialog
-        v-if="$auth.loggedIn && welcome"
-        v-model="welcome"
-        persistent
-        max-width="500"
-      >
-        <v-card v-if="$auth.loggedIn && welcome" color="teal">
-          <v-card-title>Welcome!</v-card-title>
-          <v-card-text>
-            <p class="white--text">
-              Hi
-              <span class="font-weight-bold">{{ firstName }}</span
-              >, <br />
-              <br />
-              We're sincerely happy that you joined our community! It might not
-              mean much to you, but to us, it means the world! <br />
-              <br />
-              We strongly believe that together we can build a resilient,
-              supportive and diverse community where developers and
-              entrepreneurs alike can share their products (whether that'd be an
-              app, an open-source project or a website) with the larger
-              community enabling them to further improve upon received
-              feedback.<br />
-              <br />
-              We can't wait to see what you've got to offer. Remember, we're all
-              here to support and learn from each other!<br />
-            </p>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn @click.stop="welcome = false" text icon class="ml-auto">
-              <v-icon tag="span" dense>👌🏼</v-icon>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
       <div v-if="!$apollo.loading">
         <v-row
           v-for="product in productsList.products"
@@ -91,8 +56,6 @@ export default {
   },
   data() {
     return {
-      welcome: this.$route.params.welcome || false,
-      firstName: this.$route.params.firstName || '',
       page: 0,
       loading: false
     }
---
+++ pages/welcome.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-card color="teal">
+      <v-card-title>Welcome!</v-card-title>
+      <v-card-text>
+        <p class="white--text">
+          Hi
+          <span class="font-weight-bold">{{ firstName }}</span
+          >, <br />
+          <br />
+          We're sincerely happy that you joined our community! It might not mean
+          much to you, but to us, it means the world! <br />
+          <br />
+          We strongly believe that together we can build a resilient, supportive
+          and diverse community where developers and entrepreneurs alike can
+          share their products (whether that'd be an app, an open-source project
+          or a website) with the larger community enabling them to further
+          improve upon received feedback.<br />
+          <br />
+          We can't wait to see what you've got to offer. Remember, we're all
+          here to support and learn from each other!<br />
+        </p>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
