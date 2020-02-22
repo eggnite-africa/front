168e328175a21157e9a34b01e0c0e6016a939fe3
Karim <karim_daghari@outlook.com>
2020-02-22 19:24:14 +0100

    fix: remove unused medium improvement: use for of for better performance

--- components/UserProfileSocialMediaLinks.vue
+++ components/UserProfileSocialMediaLinks.vue
@@ -1,16 +1,6 @@
 <template>
   <div>
     <v-btn
-      v-if="!!website"
-      :href="website"
-      icon
-      target="_blank"
-      rel="noreferrer noopener"
-      small
-    >
-      <v-icon>mdi-web</v-icon>
-    </v-btn>
-    <v-btn
       v-if="!!facebook"
       :href="facebook"
       icon
@@ -40,36 +30,6 @@
     >
       <v-icon>mdi-linkedin-box</v-icon>
     </v-btn>
-    <v-btn
-      v-if="!!medium"
-      :href="medium"
-      icon
-      target="_blank"
-      rel="noreferrer noopener"
-      small
-    >
-      <v-icon>mdi-medium</v-icon>
-    </v-btn>
-    <v-btn
-      v-if="!!instagram"
-      :href="instagram"
-      icon
-      target="_blank"
-      rel="noreferrer noopener"
-      small
-    >
-      <v-icon>mdi-instagram</v-icon>
-    </v-btn>
-    <v-btn
-      v-if="!!youtube"
-      :href="youtube"
-      icon
-      target="_blank"
-      rel="noreferrer noopener"
-      small
-    >
-      <v-icon>mdi-youtube</v-icon>
-    </v-btn>
   </div>
 </template>
 
@@ -84,13 +44,9 @@ export default {
   },
   data() {
     return {
-      website: null,
       facebook: null,
       twitter: null,
-      linkedin: null,
-      instagram: null,
-      youtube: null,
-      medium: null
+      linkedin: null
     }
   },
   created() {
@@ -100,23 +56,15 @@ export default {
     sortLinks() {
       const links = [...this.links]
       const is = (link, network) => link.includes(network)
-      links.forEach((link) => {
-        if (is(link, 'Facebook')) {
+      for (const link of links) {
+        if (is(link, 'facebook')) {
           this.facebook = link
-        } else if (is(link, 'Twitter')) {
+        } else if (is(link, 'twitter')) {
           this.twitter = link
-        } else if (is(link, 'LinkedIn')) {
+        } else if (is(link, 'linkedIn')) {
           this.linkedin = link
-        } else if (is(link, 'Instagram')) {
-          this.instagram = link
-        } else if (is(link, 'YouTube')) {
-          this.youtube = link
-        } else if (is(link, 'Medium')) {
-          this.medium = link
-        } else {
-          this.website = link
         }
-      })
+      }
     }
   }
 }
