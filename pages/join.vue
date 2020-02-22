5f1e0240cb35b17915ed778a2de7fe615db0c4ce
Karim <karim_daghari@outlook.com>
2020-02-22 20:45:15 +0100

    feat: streamline sign up experience
    BREAKING CHANGE: make sign up process WAY simpler

--- pages/join.vue
+++ pages/join.vue
@@ -2,100 +2,47 @@
   <v-container>
     <v-card>
       <v-card-title>Join us</v-card-title>
+      <v-card-text>
+        <form>
+          <v-text-field
+            v-model.trim="username"
+            :error-messages="usernameErrors"
+            @input="$v.username.$touch()"
+            @blur="$v.username.$touch()"
+            label="Username"
+          ></v-text-field>
 
-      <v-stepper v-model="currentStep">
-        <v-stepper-header>
-          <v-stepper-step :complete="currentStep > 1" step="1"
-            >Account Information</v-stepper-step
-          >
-          <v-divider></v-divider>
-          <v-stepper-step :complete="currentStep > 2" step="2"
-            >User Profile</v-stepper-step
-          >
-        </v-stepper-header>
+          <v-text-field
+            v-model.trim="fullName"
+            :error-messages="fullNameErrors"
+            @input="$v.fullName.$touch()"
+            @blur="$v.fullName.$touch()"
+            label="Full name"
+          ></v-text-field>
 
-        <v-stepper-items>
-          <v-stepper-content step="1">
-            <v-card-text>
-              <form>
-                <v-text-field
-                  v-model.trim="email"
-                  :error-messages="emailErrors"
-                  @input="$v.email.$touch()"
-                  @blur="$v.email.$touch()"
-                  label="Email"
-                  type="email"
-                ></v-text-field>
+          <v-text-field
+            v-model="password"
+            :error-messages="passwordErrors"
+            @input="$v.password.$touch()"
+            @blur="$v.password.$touch()"
+            label="Password"
+            type="password"
+          ></v-text-field>
 
-                <v-text-field
-                  v-model.trim="username"
-                  :error-messages="usernameErrors"
-                  @input="$v.username.$touch()"
-                  @blur="$v.username.$touch()"
-                  label="Username"
-                ></v-text-field>
-
-                <v-text-field
-                  v-model="password"
-                  :error-messages="passwordErrors"
-                  @input="$v.password.$touch()"
-                  @blur="$v.password.$touch()"
-                  label="Password"
-                  type="password"
-                ></v-text-field>
-
-                <v-text-field
-                  v-model="passwordConfirmation"
-                  :error-messages="confirmPasswordErrors"
-                  @input="$v.passwordConfirmation.$touch()"
-                  @blur="$v.passwordConfirmation.$touch()"
-                  label="Confirm password"
-                  type="password"
-                ></v-text-field>
-              </form>
-            </v-card-text>
-          </v-stepper-content>
-
-          <v-stepper-content step="2">
-            <v-card-text>
-              <user-profile-edit
-                ref="userProfile"
-                :profilePicture="profilePicture"
-                :firstName="firstName"
-                :lastName="lastName"
-                :gender="gender"
-                :birthDate="birthDate"
-                :occupation="occupation"
-                :university="university"
-                :bio="bio"
-                :socialLinks="socialLinks"
-                :country="country"
-              ></user-profile-edit>
-            </v-card-text>
-          </v-stepper-content>
-        </v-stepper-items>
-      </v-stepper>
+          <v-text-field
+            v-model="passwordConfirmation"
+            :error-messages="confirmPasswordErrors"
+            @input="$v.passwordConfirmation.$touch()"
+            @blur="$v.passwordConfirmation.$touch()"
+            label="Confirm password"
+            type="password"
+          ></v-text-field>
+        </form>
+      </v-card-text>
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn @click="previousStep()" v-show="currentStep > 1" text
-          >back</v-btn
-        >
-        <v-btn
-          ref="continueButton"
-          @click="nextStep()"
-          v-show="currentStep !== 2"
-          :color="$v.$anyError ? 'red' : 'blue'"
-          depressed
-          >Continue</v-btn
-        >
-        <v-btn
-          @click="signUp()"
-          v-if="currentStep === 2"
-          depressed
-          color="green"
-          >Join</v-btn
-        >
+        <v-btn @click="signUp()" depressed color="green">Join</v-btn>
       </v-card-actions>
     </v-card>
   </v-container>
@@ -104,28 +51,37 @@
 <script>
 import gql from 'graphql-tag'
 import { mapActions } from 'vuex'
-import { email, required, sameAs, minLength } from 'vuelidate/lib/validators'
-import UserProfileEdit from '@/components/UserProfileEditPage.vue'
+import { required, sameAs, minLength } from 'vuelidate/lib/validators'
 
 export default {
   name: 'JoinUsPage',
-  components: { UserProfileEdit },
+  data() {
+    return {
+      username: '',
+      password: '',
+      passwordConfirmation: '',
+      fullName: '',
+      firstName: '',
+      lastName: '',
+      usernameExists: false
+    }
+  },
   computed: {
-    emailErrors() {
-      const errors = []
-      if (!this.$v.email.$dirty) return errors
-      !this.$v.email.email && errors.push('email is invalid')
-      !this.$v.email.isUnique && errors.push('email is already in use')
-      !this.$v.email.required && errors.push('email is required')
-      return errors
-    },
     usernameErrors() {
       const errors = []
       if (!this.$v.username.$dirty) return errors
       !this.$v.username.required && errors.push('username is required')
       !this.$v.username.minLength &&
-        errors.push('username should be at least 2 charcters long')
+        errors.push('username should be at least 2 characters long')
       !this.$v.username.isUnique && errors.push('username is already in use')
+      return errors
+    },
+    fullNameErrors() {
+      const errors = []
+      if (!this.$v.fullName.$dirty) return errors
+      !this.$v.fullName.required && errors.push('full name is required')
+      !this.$v.fullName.minLength &&
+        errors.push('full name should be at least 3 characters long')
       return errors
     },
     passwordErrors() {
@@ -146,37 +102,7 @@ export default {
       return errors
     }
   },
-  asyncData() {
-    return {
-      currentStep: 1,
-      email: '',
-      username: '',
-      password: '',
-      passwordConfirmation: '',
-      profilePicture: '',
-      firstName: '',
-      lastName: '',
-      gender: '',
-      birthDate: '',
-      occupation: '',
-      university: '',
-      company: '',
-      bio: '',
-      country: '',
-      socialLinks: [''],
-      usernameExists: false,
-      emailExists: false
-    }
-  },
   validations: {
-    email: {
-      email,
-      required,
-      isUnique(v) {
-        if (v === '') return !this.emailExists
-        return !this.emailExists
-      }
-    },
     password: {
       required,
       minLength: minLength(8)
@@ -184,6 +110,10 @@ export default {
     passwordConfirmation: {
       required,
       sameAsPassword: sameAs('password')
+    },
+    fullName: {
+      required,
+      minLength: minLength(3)
     },
     username: {
       required,
@@ -210,58 +140,17 @@ export default {
         return this.username.length < 2
       },
       fetchPolicy: 'network-only'
-    },
-    emailExists: {
-      query: gql`
-        query checkEmailUniqueness($email: String!) {
-          emailExists: checkEmailExistance(email: $email)
-        }
-      `,
-      variables() {
-        return {
-          email: this.email
-        }
-      },
-      skip() {
-        return this.email.length < 4
-      },
-      fetchPolicy: 'network-only'
     }
   },
   methods: {
     ...mapActions({
       signIn: 'utils/login'
     }),
-    nextStep() {
-      this.$v.$touch()
-      if (!this.$v.$invalid) this.currentStep++
-    },
-    previousStep() {
-      this.currentStep--
-    },
     async login() {
       const [username, password] = [this.username, this.password]
       await this.signIn({ username, password })
     },
     async signUp() {
-      const userProfile = this.$refs.userProfile
-      const userProfileIsValid = userProfile.isValid()
-      if ((this.$v.$touch() && this.$v.$invalid) || !userProfileIsValid) return
-
-      const {
-        profilePicture,
-        firstName,
-        lastName,
-        gender,
-        birthDate,
-        occupation,
-        university,
-        company,
-        bio,
-        socialLinks,
-        country
-      } = userProfile.getProfileInfo()
-
       await this.$apollo
         .mutate({
           mutation: gql`
@@ -278,17 +167,8 @@ export default {
               email: this.email,
               password: this.password,
               profile: {
-                profilePicture,
-                firstName,
-                lastName,
-                gender,
-                birthDate,
-                occupation,
-                bio,
-                university,
-                company,
-                socialLinks,
-                country
+                firstName: this.firstName,
+                lastName: this.lastName
               }
             }
           }
@@ -297,7 +177,7 @@ export default {
           await this.login()
           this.$router.push({
             name: 'index',
-            params: { welcome: true, firstName }
+            params: { welcome: true, firstName: this.firstName }
           })
         })
     }
