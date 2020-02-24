6fbdc00d72e226856f6ac6ca1c099e7d47bd7d0b
Karim <karim_daghari@outlook.com>
2020-02-24 22:01:42 +0100

    feat: add data retrieval

--- pages/p/_name/edit.vue
+++ pages/p/_name/edit.vue
@@ -12,7 +12,7 @@
     <v-card>
       <v-card-title v-text="productName"></v-card-title>
       <v-card-text>
-        <product-edit ref="productEdit" :is-edit="true" :on-submit="onSubmit">
+        <product-edit :on-submit="onSubmit">
           Update
         </product-edit>
       </v-card-text>
@@ -21,8 +21,9 @@
 </template>
 
 <script>
+import gql from 'graphql-tag'
+import { mapMutations } from 'vuex'
 import ProductEdit from '@/components/ProductPostPage.vue'
-
 export default {
   components: {
     ProductEdit
@@ -42,10 +43,34 @@ export default {
       return this.$route.params.name.replace(/-/g, ' ')
     }
   },
+  asyncData() {
+    return {
+      product: {
+        id: '',
+        name: '',
+        tagline: '',
+        description: '',
+        media: {
+          logo: '',
+          pictures: ['']
+        },
+        makers: [{ id: '' }],
+        links: {
+          website: '',
+          github: '',
+          appStore: '',
+          playStore: ''
+        }
+      }
+    }
+  },
   methods: {
+    ...mapMutations({
+      updateField: 'product/updateField'
+    }),
     async onSubmit() {
       try {
-        await this.$refs.productEdit.updateProduct()
+        await this.updateProduct()
         this.message.err = false
         this.message.icon = 'mdi-check'
         this.message.text = 'The product was successfully updated!'
@@ -56,7 +81,70 @@ export default {
       } finally {
         this.message.display = true
       }
+    },
+    populateStore(product) {
+      function _normalizeData(product) {
+        const normalizedProduct = {}
+        let keys = Object.keys(product)
+        keys = keys.filter(
+          (k) => k !== 'makers' && k !== 'links' && k !== 'media'
+        )
+        for (const key of keys) {
+          normalizedProduct[key] = product[key]
+        }
+        const links = Object.entries(product.links)
+        for (const [link, value] of links) {
+          normalizedProduct[link] = value
+        }
+        for (const key of ['logo', 'pictures']) {
+          normalizedProduct[key] = product.media[key]
+        }
+        const makers = product.makers.map(({ id }) => id)
+        normalizedProduct.makers = makers
+        return normalizedProduct
+      }
+
+      const normalizedProduct = _normalizeData(this.product)
+      const keys = Object.keys(normalizedProduct)
+      for (const key of keys) {
+        this.updateField({ fieldName: key, value: normalizedProduct[key] })
+      }
     }
+  },
+  apollo: {
+    product: {
+      query: gql`
+        query fetchProductForUpdate($name: String!) {
+          product(name: $name) {
+            id
+            name
+            tagline
+            description
+            media {
+              logo
+              pictures
+            }
+            makers {
+              id
+            }
+            links {
+              website
+              github
+              appStore
+              playStore
+            }
+          }
+        }
+      `,
+      variables() {
+        return {
+          name: this.productName
+        }
+      }
+    }
+  },
+  created() {
+    this.populateStore(this.product)
   },
   middleware: ['auth', 'isProductOwner'],
   head() {
