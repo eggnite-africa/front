6a81cba1b08e9d8491df45a4b1f9326b9ea343dc
Karim <karim_daghari@outlook.com>
2020-03-01 20:44:24 +0100

    add competitions display

---
+++ components/CompetitionsPageSingleItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card nuxt to="/about">
+    <v-list-item class="pt-1">
+      <v-list-item-avatar color="grey">
+        <v-img :src="logo"></v-img>
+      </v-list-item-avatar>
+      <v-list-item-content>
+        <v-list-item-title v-text="name" class="headline"></v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-card-text v-text="description"> </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    logo: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- pages/competitions/index.vue
+++ pages/competitions/index.vue
@@ -5,10 +5,63 @@
         Add competition
       </v-btn>
     </v-row>
-    <v-row align="center" justify="center">Competitions here...</v-row>
+    <v-row v-if="!$apollo.loading" align="center">
+      <v-col
+        v-for="competition in competitionsList.competitions"
+        :key="competition.name"
+        cols="12"
+        sm="6"
+      >
+        <competition-item
+          :logo="competition.logo"
+          :name="competition.name"
+          :description="competition.description"
+        ></competition-item>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-export default {}
+import gql from 'graphql-tag'
+import CompetitionItem from '@/components/CompetitionsPageSingleItem.vue'
+export default {
+  components: {
+    CompetitionItem
+  },
+  asyncData() {
+    return {
+      competitionsList: {
+        hasMore: null,
+        competitions: [
+          {
+            name: '',
+            logo: '',
+            description: ''
+          }
+        ]
+      }
+    }
+  },
+  apollo: {
+    competitionsList: {
+      query: gql`
+        query fetchAllCompetitions($page: Int!) {
+          competitionsList(page: $page) {
+            hasMore
+            competitions {
+              name
+              logo
+              description
+            }
+          }
+        }
+      `,
+      variables: {
+        page: 0
+      },
+      fetchPolicy: 'network-only'
+    }
+  }
+}
 </script>
