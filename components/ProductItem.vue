f854465618aeff75cee9488bdca4029d1e611111
Karim <karim_daghari@outlook.com>
2020-03-03 22:38:00 +0100

    refactor: decompose settings buttons

--- components/ProductItem.vue
+++ components/ProductItem.vue
@@ -19,7 +19,14 @@
           </div>
         </v-list-item-content>
         <v-list-item-action class="hidden-sm-and-up justify-center">
+          <product-item-action-buttons-settings
+            v-if="isInSettings"
+            :product-edit-link="productEditLink"
+            :product-id="id"
+          >
+          </product-item-action-buttons-settings>
           <product-item-action-buttons
+            v-else
             :product-id="id"
             :comments-count="commentsLength"
             :votes-count="votes.length"
@@ -29,7 +36,14 @@
         </v-list-item-action>
       </v-list-item-content>
       <v-list-item-action class="hidden-xs-only">
+        <product-item-action-buttons-settings
+          v-if="isInSettings"
+          :product-edit-link="productEditLink"
+          :product-id="id"
+        >
+        </product-item-action-buttons-settings>
         <product-item-action-buttons
+          v-else
           :product-id="id"
           :comments-count="commentsLength"
           :votes-count="votes.length"
@@ -38,91 +52,19 @@
         ></product-item-action-buttons>
       </v-list-item-action>
     </v-list-item>
-
-    <!-- <v-col cols="5" sm="2" class="hidden-xs-only">
-        <v-card-actions>
-          <v-row dense>
-            <v-col cols="12">
-              <template v-if="!isInSettings">
-                <v-btn
-                  @click.prevent="upvote()"
-                  :outlined="!hasVoted"
-                  color="orange"
-                  block
-                  height="50"
-                >
-                  <div class="flex-md-column">
-                    <v-icon>mdi-arrow-up-thick</v-icon>
-                    <div v-text="votes.length"></div>
-                  </div>
-                </v-btn>
-              </template>
-              <template v-else>
-                <v-btn
-                  :to="productEditLink"
-                  color="indigo"
-                  block
-                  depressed
-                  nuxt
-                >
-                  Edit
-                </v-btn>
-              </template>
-            </v-col>
-            <v-col cols="12">
-              <template v-if="!isInSettings">
-                <v-btn :to="commentsSection" nuxt outlined block>
-                  <v-icon left>mdi-comment</v-icon>
-                  <span v-text="commentsLength"></span>
-                </v-btn>
-              </template>
-              <template v-else>
-                <v-btn @click.stop="dialog = true" color="red" depressed block>
-                  Delete
-                </v-btn>
-                <v-dialog v-model="dialog" persistent max-width="290">
-                  <v-card>
-                    <v-card-title class="headline"
-                      >Delete {{ name }}</v-card-title
-                    >
-
-                    <v-card-text>
-                      Are you sure you want to do this?
-                    </v-card-text>
-
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-
-                      <v-btn @click="dialog = false" text>
-                        No
-                      </v-btn>
-
-                      <v-btn
-                        @click.stop="deleteProduct(id)"
-                        color="red darken-1"
-                        text
-                      >
-                        Yes
-                      </v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
-              </template>
-            </v-col>
-          </v-row>
-        </v-card-actions>
-      </v-col> -->
   </v-card>
 </template>
 
 <script>
 import ProductMakerAvatar from '@/components/ProductItemMakerAvatar.vue'
 import ProductItemActionButtons from '@/components/ProductItemActionButtons.vue'
+import ProductItemActionButtonsSettings from '@/components/ProductItemActionButtonsSettings.vue'
 export default {
   name: 'ProductItem',
   components: {
     ProductMakerAvatar,
-    ProductItemActionButtons
+    ProductItemActionButtons,
+    ProductItemActionButtonsSettings
   },
   props: {
     id: {
@@ -189,23 +131,6 @@ export default {
     }
   },
   methods: {
-    // async deleteProduct(id) {
-    //   await this.$apollo
-    //     .mutate({
-    //       mutation: gql`
-    //         mutation deleteProduct($id: ID!) {
-    //           deleteProduct(id: $id)
-    //         }
-    //       `,
-    //       variables: {
-    //         id
-    //       }
-    //     })
-    //     .then(() => {
-    //       this.dialog = false
-    //       this.$router.go(0)
-    //     })
-    // },
     makerAvatar(index) {
       const maker = this.makers[index]
       return maker.profile.picture
