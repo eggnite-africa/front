ecc115835678d2c3d2dba0fe0d82082a2879a7c7
Karim <karim_daghari@outlook.com>
2020-03-18 15:01:04 +0100

    add posting interface

---
+++ components/PitchPostPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <form @submit.prevent="beforeSubmit">
+      <v-row dense>
+        <v-col cols="12">
+          <v-textarea v-model="pContent" outlined label="Content"></v-textarea>
+        </v-col>
+      </v-row>
+
+      <v-row dense>
+        <v-spacer></v-spacer>
+        <v-btn color="secondary" type="submit" depressed><slot></slot></v-btn>
+      </v-row>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PitchPostPage',
+  props: {
+    onSubmit: {
+      type: Function,
+      required: true
+    },
+    content: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      pContent: this.content
+    }
+  },
+  methods: {
+    beforeSumbit() {
+      const updatedPitch = {
+        content: this.pContent
+      }
+      this.$emit('update-pitch', updatedPitch)
+      this.onSubmit()
+    }
+  }
+}
+</script>
---
+++ pages/i/_id/edit.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-container>
+    <v-card v-if="!$apollo.loading">
+      <v-card-title>Edit: {{ pitch.title }}</v-card-title>
+      <v-card-text>
+        <pitch-post-page
+          :content="pitch.content"
+          @update-pitch="updatePitch($event)"
+        >
+          update
+        </pitch-post-page>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+import PitchPostPage from '@/components/PitchPostPage.vue'
+export default {
+  name: 'PitchEditPage',
+  components: {
+    PitchPostPage
+  },
+  asyncData() {
+    return {
+      pitch: {
+        id: '',
+        title: '',
+        content: ''
+      }
+    }
+  },
+  apollo: {
+    pitch: {
+      query: gql`
+        query fetchPitchForUpdate($id: ID!) {
+          pitch(id: $id) {
+            id
+            title
+            content
+          }
+        }
+      `,
+      variables() {
+        return {
+          id: this.$route.params.id
+        }
+      }
+    }
+  },
+  methods: {
+    async updatePitch(updatedPitchPayload) {
+      await this.$apollo.mutate({
+        mutation: gql`
+          mutation updatePitch($updatedPitch: UpdatedPitchInput!) {
+            updatePitch(updatedPitch: $updatedPitch) {
+              id
+              content
+            }
+          }
+        `,
+        variables: {
+          updatedPitch: {
+            userId: this.$auth.user.id,
+            id: this.pitch.id,
+            ...updatedPitchPayload
+          }
+        }
+      })
+    }
+  }
+}
+</script>
