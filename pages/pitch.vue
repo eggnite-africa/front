608e6a139203e56c7cf6ed32fac62a5c32be96c7
Karim <karim_daghari@outlook.com>
2020-03-18 17:02:31 +0100

    add pitch creation page

--- pages/pitch.vue
+++ pages/pitch.vue
@@ -1,11 +1,49 @@
 <template>
   <v-container>
-    Pitch list here...
+    <v-card>
+      <v-card-title>Add Idea</v-card-title>
+      <v-card-text>
+        <pitch-post-page @update-pitch="addPitch($event)" :onSubmit="addPitch"
+          >add</pitch-post-page
+        >
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+import PitchPostPage from '@/components/PitchPostPage.vue'
 export default {
-  name: 'PitchPage'
+  name: 'PitchPage',
+  components: {
+    PitchPostPage
+  },
+  methods: {
+    addPitch(newPitch) {
+      this.$apollo
+        .mutate({
+          mutation: gql`
+            mutation addPitch($newPitch: NewPitchInput!) {
+              addPitch(newPitch: $newPitch) {
+                id
+              }
+            }
+          `,
+          variables: {
+            newPitch
+          }
+        })
+        .then(({ data: { addPitch } }) => {
+          const { id } = addPitch
+          this.$router.replace({
+            name: 'p-id',
+            params: {
+              id
+            }
+          })
+        })
+    }
+  }
 }
 </script>
