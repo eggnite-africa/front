26be6dcb7cec7c6cb353b348f57a91506779f012
Karim <karim_daghari@outlook.com>
2020-03-19 18:38:52 +0100

    fix weird bug where btns are displayed twice

--- components/PitchItemActionButtonsSettings.vue
+++ components/PitchItemActionButtonsSettings.vue
@@ -1,34 +1,36 @@
 <template>
   <div>
-    <v-btn :to="`/i/${pitchId}/edit`" color="indigo" depressed nuxt small>
-      <v-icon small class="hidden-sm-and-up">mdi-wrench</v-icon>
-      <span class="hidden-xs-only">Edit</span>
-    </v-btn>
-    <v-btn @click.prevent="dialog = true" color="red" depressed small>
-      <v-icon small class="hidden-sm-and-up">mdi-close-circle</v-icon>
-      <span class="hidden-xs-only">Delete</span>
-    </v-btn>
-    <v-dialog v-model="dialog" persistent max-width="290">
-      <v-card>
-        <v-card-title class="headline">Delete idea</v-card-title>
+    <client-only>
+      <v-btn :to="`/i/${pitchId}/edit`" color="indigo" depressed nuxt small>
+        <v-icon small class="hidden-sm-and-up">mdi-wrench</v-icon>
+        <span class="hidden-xs-only">Edit</span>
+      </v-btn>
+      <v-btn @click.prevent="dialog = true" color="red" depressed small>
+        <v-icon small class="hidden-sm-and-up">mdi-close-circle</v-icon>
+        <span class="hidden-xs-only">Delete</span>
+      </v-btn>
+      <v-dialog v-model="dialog" persistent max-width="290">
+        <v-card>
+          <v-card-title class="headline">Delete idea</v-card-title>
 
-        <v-card-text>
-          Are you sure you want to delete this idea?
-        </v-card-text>
+          <v-card-text>
+            Are you sure you want to delete this idea?
+          </v-card-text>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
+          <v-card-actions>
+            <v-spacer></v-spacer>
 
-          <v-btn @click="dialog = false" text>
-            No
-          </v-btn>
+            <v-btn @click="dialog = false" text>
+              No
+            </v-btn>
 
-          <v-btn @click.stop="deletePitch(pitchId)" color="red darken-1" text>
-            Yes
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+            <v-btn @click.stop="deletePitch(pitchId)" color="red darken-1" text>
+              Yes
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </client-only>
   </div>
 </template>
 
