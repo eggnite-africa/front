dce7ba17a017dfde52239b34909cec1ad6023810
Karim <karim_daghari@outlook.com>
2020-03-19 10:43:32 +0100

    add validation

--- components/PitchPostPage.vue
+++ components/PitchPostPage.vue
@@ -3,14 +3,31 @@
     <form @submit.prevent="beforeSubmit()">
       <v-row dense>
         <v-col cols="12">
-          <v-text-field v-model="pTitle" outlined label="Title"></v-text-field>
+          <v-text-field
+            v-model="pTitle"
+            :error-messages="titleErrors"
+            @input="$v.pTitle.$touch()"
+            @blur="$v.pTitle.$touch()"
+            outlined
+            label="Title"
+          ></v-text-field>
         </v-col>
         <v-col cols="12">
-          <v-textarea v-model="pProblem" outlined label="Problem"></v-textarea>
+          <v-textarea
+            v-model="pProblem"
+            :error-messages="problemErrors"
+            @input="$v.pProblem.$touch()"
+            @blur="$v.pProblem.$touch()"
+            outlined
+            label="Problem"
+          ></v-textarea>
         </v-col>
         <v-col cols="12">
           <v-textarea
             v-model="pSolution"
+            :error-messages="solutionErrors"
+            @input="$v.pSolution.$touch()"
+            @blur="$v.pSolution.$touch()"
             outlined
             label="Solution"
           ></v-textarea>
@@ -18,6 +35,9 @@
         <v-col cols="12">
           <v-text-field
             v-model="pSkills"
+            :error-messages="skillsErrors"
+            @input="$v.pSkills.$touch()"
+            @blur="$v.pSkills.$touch()"
             outlined
             label="Qualifications"
           ></v-text-field>
@@ -25,6 +45,9 @@
         <v-col cols="12">
           <v-text-field
             v-model="pNeeds"
+            :error-messages="needsErrors"
+            @input="$v.pNeeds.$touch()"
+            @blur="$v.pNeeds.$touch()"
             outlined
             label="Needs"
             hint="What are you looking for? Collaborators? Financiers? Etc..."
@@ -41,6 +64,7 @@
 </template>
 
 <script>
+import { required } from 'vuelidate/lib/validators'
 export default {
   name: 'PitchPostPage',
   props: {
@@ -78,8 +102,63 @@ export default {
       pNeeds: this.needs
     }
   },
+  computed: {
+    titleErrors() {
+      const errors = []
+      if (!this.$v.pTitle.$dirty) return errors
+      !this.$v.pTitle.required && errors.push('the idea should have a name')
+      return errors
+    },
+    problemErrors() {
+      const errors = []
+      if (!this.$v.pProblem.$dirty) return errors
+      !this.$v.pProblem.required &&
+        errors.push('the idea should address a problem... duh!')
+      return errors
+    },
+    solutionErrors() {
+      const errors = []
+      if (!this.$v.pSolution.$dirty) return errors
+      !this.$v.pSolution.required &&
+        errors.push('a problem without a solution is called a complaint.')
+      return errors
+    },
+    skillsErrors() {
+      const errors = []
+      if (!this.$v.pSkills.$dirty) return errors
+      !this.$v.pSkills.required &&
+        errors.push('ok, but like... What do you know?')
+      return errors
+    },
+    needsErrors() {
+      const errors = []
+      if (!this.$v.pNeeds.$dirty) return errors
+      !this.$v.pNeeds.required && errors.push('you should specify your needs')
+      return errors
+    }
+  },
+  validations: {
+    pTitle: {
+      required
+    },
+    pProblem: {
+      required
+    },
+    pSolution: {
+      required
+    },
+    pSkills: {
+      required
+    },
+    pNeeds: {
+      required
+    }
+  },
   methods: {
     beforeSubmit() {
+      this.$v.$touch()
+      if (this.$v.$invalid) return
+
       const updatedPitch = {
         title: this.pTitle,
         problem: this.pProblem,
